4969d29c857bcd29ca03f82f6904ff690535857f
Pranav Dwivedi <pranavdwivedi@Pranavs-MacBook-Pro.local>
2026-02-22 19:05:12 +1100

    Auto-update gallery

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -9,169 +9,505 @@ export interface PortfolioItem {
 export const PORTFOLIO_ITEMS: PortfolioItem[] = [
   {
     "id": 1,
-    "url": "/gallery/DSC00105.webp",
-    "title": "DSC00105",
+    "url": "/gallery/'Til Next Time.webp",
+    "title": "'Til Next Time",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 2,
-    "url": "/gallery/DSC00111.webp",
-    "title": "DSC00111",
+    "url": "/gallery/A Stitch in the Blue.webp",
+    "title": "A Stitch In The Blue",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 3,
-    "url": "/gallery/DSC00112.webp",
-    "title": "DSC00112",
+    "url": "/gallery/Afternoon at the Opera.webp",
+    "title": "Afternoon At The Opera",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 4,
-    "url": "/gallery/DSC00113.webp",
-    "title": "DSC00113",
+    "url": "/gallery/Archway to the Ether.webp",
+    "title": "Archway To The Ether",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 5,
-    "url": "/gallery/DSC00117.webp",
-    "title": "DSC00117",
+    "url": "/gallery/Between Blocks.webp",
+    "title": "Between Blocks",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 6,
-    "url": "/gallery/DSC00122.webp",
-    "title": "DSC00122",
+    "url": "/gallery/Brutalist Bird of Prey.webp",
+    "title": "Brutalist Bird Of Prey",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 7,
-    "url": "/gallery/DSC00234.webp",
-    "title": "DSC00234",
+    "url": "/gallery/Burning Stratos.webp",
+    "title": "Burning Stratos",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 8,
-    "url": "/gallery/DSC00272.webp",
-    "title": "DSC00272",
+    "url": "/gallery/Canberra After Dark.webp",
+    "title": "Canberra After Dark",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 9,
-    "url": "/gallery/DSC00418.webp",
-    "title": "DSC00418",
+    "url": "/gallery/Canopy of Colours.webp",
+    "title": "Canopy Of Colours",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 10,
-    "url": "/gallery/DSC00584.webp",
-    "title": "DSC00584",
+    "url": "/gallery/Clear Skies Ahead.webp",
+    "title": "Clear Skies Ahead",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 11,
-    "url": "/gallery/DSC00590.webp",
-    "title": "DSC00590",
+    "url": "/gallery/Cliffside Swell.webp",
+    "title": "Cliffside Swell",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 12,
-    "url": "/gallery/DSC00600-2.webp",
-    "title": "DSC00600 2",
+    "url": "/gallery/Commute Reflections.webp",
+    "title": "Commute Reflections",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 13,
-    "url": "/gallery/DSC00601.webp",
-    "title": "DSC00601",
+    "url": "/gallery/Cul-de-sac Clouds.webp",
+    "title": "Cul De Sac Clouds",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 14,
-    "url": "/gallery/DSC00605.webp",
-    "title": "DSC00605",
+    "url": "/gallery/Dharma in the Leaves.webp",
+    "title": "Dharma In The Leaves",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 15,
-    "url": "/gallery/DSC00608.webp",
-    "title": "DSC00608",
+    "url": "/gallery/Dusk Commute.webp",
+    "title": "Dusk Commute",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 16,
-    "url": "/gallery/DSC00610.webp",
-    "title": "DSC00610",
+    "url": "/gallery/Dusk Over the Transit.webp",
+    "title": "Dusk Over The Transit",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 17,
-    "url": "/gallery/DSC00630.webp",
-    "title": "DSC00630",
+    "url": "/gallery/Fractured Moonlight.webp",
+    "title": "Fractured Moonlight",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 18,
-    "url": "/gallery/DSC00631.webp",
-    "title": "DSC00631",
+    "url": "/gallery/Framing the Foothills.webp",
+    "title": "Framing The Foothills",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 19,
-    "url": "/gallery/DSC00642.webp",
-    "title": "DSC00642",
+    "url": "/gallery/Ganjing After Downpour.webp",
+    "title": "Ganjing After Downpour",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 20,
-    "url": "/gallery/SAM_0011.webp",
-    "title": "SAM 0011",
+    "url": "/gallery/Gothic Twilight.webp",
+    "title": "Gothic Twilight",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 21,
-    "url": "/gallery/SAM_0020.webp",
-    "title": "SAM 0020",
+    "url": "/gallery/Gritty Academia.webp",
+    "title": "Gritty Academia",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
     "id": 22,
-    "url": "/gallery/SAM_0043.webp",
-    "title": "SAM 0043",
+    "url": "/gallery/Guidance in the Dark.webp",
+    "title": "Guidance In The Dark",
     "preset": "Custom Edit",
-    "orientation": "landscape"
+    "orientation": "portrait"
   },
   {
     "id": 23,
-    "url": "/gallery/SAM_0055.webp",
-    "title": "SAM 0055",
+    "url": "/gallery/Heritage in Red Brick.webp",
+    "title": "Heritage In Red Brick",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 24,
+    "url": "/gallery/Imperial Arches.webp",
+    "title": "Imperial Arches",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 25,
+    "url": "/gallery/Kitten in the Canopy.webp",
+    "title": "Kitten In The Canopy",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 26,
+    "url": "/gallery/Layered Silence.webp",
+    "title": "Layered Silence",
     "preset": "Custom Edit",
     "orientation": "landscape"
   },
   {
-    "id": 24,
-    "url": "/gallery/SAM_0057.webp",
-    "title": "SAM 0057",
+    "id": 27,
+    "url": "/gallery/Low-Angle Transit.webp",
+    "title": "Low Angle Transit",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 28,
+    "url": "/gallery/Market Momentum.webp",
+    "title": "Market Momentum",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 29,
+    "url": "/gallery/Midnight at the Quay.webp",
+    "title": "Midnight At The Quay",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 30,
+    "url": "/gallery/Nocturnal Canberra.webp",
+    "title": "Nocturnal Canberra",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 31,
+    "url": "/gallery/Nocturnal Transit.webp",
+    "title": "Nocturnal Transit",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 32,
+    "url": "/gallery/Obsidian Arteries.webp",
+    "title": "Obsidian Arteries",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 33,
+    "url": "/gallery/Origami Ascent.webp",
+    "title": "Origami Ascent",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 34,
+    "url": "/gallery/Portal No.1.webp",
+    "title": "Portal No.1",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 35,
+    "url": "/gallery/Salmon and Jade.webp",
+    "title": "Salmon And Jade",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 36,
+    "url": "/gallery/Sandstone Angler.webp",
+    "title": "Sandstone Angler",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 37,
+    "url": "/gallery/Serrated Skies.webp",
+    "title": "Serrated Skies",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 38,
+    "url": "/gallery/Shadows of the CBD.webp",
+    "title": "Shadows Of The CBD",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 39,
+    "url": "/gallery/Showroom Shadows.webp",
+    "title": "Showroom Shadows",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 40,
+    "url": "/gallery/Silhoettes and Sky.webp",
+    "title": "Silhoettes And Sky",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 41,
+    "url": "/gallery/Springtime Canvas.webp",
+    "title": "Springtime Canvas",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 42,
+    "url": "/gallery/Stacked Suburbia.webp",
+    "title": "Stacked Suburbia",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 43,
+    "url": "/gallery/Stardust Veil.webp",
+    "title": "Stardust Veil",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 44,
+    "url": "/gallery/Steel Symphony.webp",
+    "title": "Steel Symphony",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 45,
+    "url": "/gallery/Subtropical Reach.webp",
+    "title": "Subtropical Reach",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 46,
+    "url": "/gallery/Suburban Surf Watch.webp",
+    "title": "Suburban Surf Watch",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 47,
+    "url": "/gallery/The Amber Awning.webp",
+    "title": "The Amber Awning",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 48,
+    "url": "/gallery/The Antenna's Watch.webp",
+    "title": "The Antenna'S Watch",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 49,
+    "url": "/gallery/The Arched Abyss.webp",
+    "title": "The Arched Abyss",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 50,
+    "url": "/gallery/The Bricklot Horizon.webp",
+    "title": "The Bricklot Horizon",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 51,
+    "url": "/gallery/The Complex Courtyard.webp",
+    "title": "The Complex Courtyard",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 52,
+    "url": "/gallery/The Concrete Chasm.webp",
+    "title": "The Concrete Chasm",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 53,
+    "url": "/gallery/The Concrete Corridor.webp",
+    "title": "The Concrete Corridor",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 54,
+    "url": "/gallery/The Crimson Connector.webp",
+    "title": "The Crimson Connector",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 55,
+    "url": "/gallery/The Eastern Suburbs Fade.webp",
+    "title": "The Eastern Suburbs Fade",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 56,
+    "url": "/gallery/The Edge of the Shelf.webp",
+    "title": "The Edge Of The Shelf",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 57,
+    "url": "/gallery/The Midnight Totem.webp",
+    "title": "The Midnight Totem",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 58,
+    "url": "/gallery/The Modern Ascent.webp",
+    "title": "The Modern Ascent",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 59,
+    "url": "/gallery/The Modern Meridian.webp",
+    "title": "The Modern Meridian",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 60,
+    "url": "/gallery/The Molten Twilight.webp",
+    "title": "The Molten Twilight",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 61,
+    "url": "/gallery/The Mossy Runnel.webp",
+    "title": "The Mossy Runnel",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 62,
+    "url": "/gallery/The Obsidian Tide.webp",
+    "title": "The Obsidian Tide",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 63,
+    "url": "/gallery/The Pacifist and the Payload.webp",
+    "title": "The Pacifist And The Payload",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 64,
+    "url": "/gallery/The Sculpted Shade.webp",
+    "title": "The Sculpted Shade",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 65,
+    "url": "/gallery/The Tangerine Rod.webp",
+    "title": "The Tangerine Rod",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 66,
+    "url": "/gallery/The Violet Hour.webp",
+    "title": "The Violet Hour",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 67,
+    "url": "/gallery/The Winter's Bud.webp",
+    "title": "The Winter'S Bud",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 68,
+    "url": "/gallery/Timeless Steel.webp",
+    "title": "Timeless Steel",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 69,
+    "url": "/gallery/To Infinity and the Rear Window.webp",
+    "title": "To Infinity And The Rear Window",
+    "preset": "Custom Edit",
+    "orientation": "portrait"
+  },
+  {
+    "id": 70,
+    "url": "/gallery/Twilight Promenade.webp",
+    "title": "Twilight Promenade",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 71,
+    "url": "/gallery/Walker's Discovery.webp",
+    "title": "Walker'S Discovery",
+    "preset": "Custom Edit",
+    "orientation": "landscape"
+  },
+  {
+    "id": 72,
+    "url": "/gallery/Widebody Under Sodium.webp",
+    "title": "Widebody Under Sodium",
     "preset": "Custom Edit",
     "orientation": "landscape"
   }
